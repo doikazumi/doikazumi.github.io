42f5af76e0b35d3228c33989ce8bef4be8921529
野間涼司 <hw15a153@oecu.jp>
2017-05-07 01:01:57 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,33 @@
+Rails.application.routes.draw do
+  get 'crops/butterbur'
+
+  get 'crops/grape'
+
+  get 'crops/chestnut'
+
+  get 'crops/paddyfield'
+
+  get 'crops/tomato'
+
+  get 'crops/watermelon'
+
+  get 'crops/sweetpotato'
+
+  root 'pages#home'
+  get 'pages/home'
+
+  get 'pages/profile'
+
+  get 'pages/works'
+
+  get 'pages/contact'
+
+  get 'pages/access'
+
+  get 'pages/link'
+  
+  get 'pages/about'
+
+  get 'pages/help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
